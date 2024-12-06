fc0d2d1890b390d19e64c2089f5f07b3bf167949
aniket007sp <151561732+aniket007sp@users.noreply.github.com>
2024-12-06 12:40:05 +0530

    Create termsConditions.js

---
+++ src/data/termsConditions.js
@@ -0,0 +1,209 @@
+export const termsConditions = {
+    
+        "Accessorize London": null
+    ,
+
+        "Acer": "Cancellations will void the entire sale.  / Categories: Laptops, Desktops, Chromebooks, Monitors. / Orders below 4k not payable. / Any orders which can be attributed to Acer partners (Offline acer stores placing orders for fulfilment on the Acer India Website) will not be eligible for the payout."
+    ,
+    
+        "Acwo": null
+    ,
+    
+        "Adidas": null
+    ,
+    
+        "Air India": "International Flights: 315\u20b9\nDomestic Flights: 94\u20b9"
+    ,
+    
+        "Airtel Payments Bank": "Creating an Account in Airtel Payments BankUser Journey Flow: Clicks> App install> Complete registration> CKYC Sign up ( Aadhar & other details)>Face recognition>KYC Details fetch>wallet transaction> Successful CKYC Account open"
+    ,
+    
+        "AMEX Platinum Travel Credit Card": "Card Disptach / Applicant should be from one of these cities: Ahmedabad, Bangalore, Chandigarh, Chennai, Coimbatore, Delhi/NCR, Hyderabad, Indore, Jaipur, Kolkata, Mumbai, Pune, Surat, Vadodara & Visakhapatnam / Income: 6 lakh+ Per Annum"
+    ,
+    
+        "Appreciate wealth": " The user has to Install  the app and complete the further process"
+    ,
+    
+        "Aqualens": null
+    ,
+    
+        "Arata": " Cancelled/returned products will not be counted for Cash Rewards."
+    ,
+    
+        "AU Bank Credit Card": "25+years, 6 Lacs & above / Salary bar to be raised to 45-50K+ and Cibil 750+ for better approval. / Credit card disbursal"
+    ,
+    
+        "Axis bank CC": "Customer (Criteria: Net Salary min 15K & above, Self-employed Rs 4 lakh latest ITR) for credit card basis its benefits, value proposition & need to assist to complete IPA journey (digital lead generation process). IPA journey is very short & simple, The customer needs to fill in his personal & occupational details in journey , need to select credit card & then needs to submit a digital application. \nBased on IPA journey output either application would be approved or declined. For approved cases\nIf only KYC needs to be collected then the Partner needs to guide the customer to complete the Video KYC process, the card would be booked in a short span under this process. \nIf Income + KYC needs to be collected from the customer then these leads would be taken care of by Axis Bank, Bank Centralized pickup team would fix an appointment with the customer & will complete the documentation process digitally"
+    ,
+    
+        "Bajaj Finserv": " KYC Verified"
+    ,
+    
+        "Blackberrys New": null
+    ,
+    
+        "Bliss Club": null
+    ,
+    
+        "Boldcare": null
+    ,
+    
+        "Candere": "Regions:-\n- North: Delhi, Lucknow, Noida, Faridabad, Gurgaun, Chandigharh\n- South: Bangalore, Hyderabad, Cochin, Chennai,\n- West: Mumbai, New Mumbai, Thane, Pune\n- East: Kolkata, Guwahati\nMaximum Payout per transaction is Rs.4500.\nNo payout on a sale of loose solitaire, gift cards and gold coin/frames/bars.\nValidation would be done after 45 days of the order placed (As it takes roughly 14-15 days for the product to be delivered since our product is \"Make to order jewellery  and the customer has 30 days return policy option)\nLoose solitaire, gift cards and gold coin/frames/bars\t-\t0\u20b9\nGold\t100000.00\u20b9\n-\t3.2%\nDiamond\t62500.00\u20b9\n-\t5.1%"
+    ,
+    
+        "Credilio": "Payable event is card activation."
+    ,
+    
+        "Dyson Direct": "Transaction that are below 17K will not be billed"
+    ,
+    
+        "ET Prime": null
+    ,
+    
+        "Fast & Up": null
+    ,
+    
+        "Feedsmart": null
+    ,
+    
+        "Fiona Diamonds": null
+    ,
+    
+        "Fix Derma": null
+    ,
+    
+        "Foxtale": "New User: 12.6%\nOld User: 6.3%"
+    ,
+    
+        "Furlenco Rentals": "Region - Bengaluru, Hyderabad, Pune, Thane, Mumbai, Navi Mumbai, Chennai, Gurgaon, Faridabad, Ghaziabad, Delhi, New Delhi, Noida, Greater Noida.\nPayout only on successfully delivered\nPurchase: 385\u20b9-490\u20b9\nMobile App Purchase: 385\u20b9-490\u20b9"
+    ,
+    
+        "Ganga Fashions": null
+    ,
+    
+        "Giva": "Payout only on successfully delivered\n Gold + Diamond + Made to Order\t3.10%\nDefault rate (silver)\t9.50%\nCandles and Perfumes\t9.50%"
+    ,
+    
+        "HDFC Credit Card": "Salary: 2.4 L plus+\nSuccessful Card Dispatch"
+    ,
+    
+        "HK Vitals": null
+    ,
+    
+        "HSBC Cashback CC": "Eligibility Criteria\n- Age: 26-45 M/F\n- Residence city: Mumbai, Pune, Bangalore, Hyderabad, New Delhi, Noida, Gurgaon, Chennai, Kolkata\n- Annual income: Above 5 lakhs\n- Occupation: Corporate salaried only. Self-employed or users working in proprietorship/partnership firm are a direct reject.\n- Should be an existing credit card holder preferably holding Amex, Stanc, Citi, Axis, ICICI or HDFC credit card\n- Cibil score: 720+\nReward on Successful Disbursal"
+    ,
+    
+        "Hyugalife": "New User: 9.50%\nRepeat User: 5.00%"
+    ,
+    
+        "IndusInd Credit Card": "Every card dispatch to the user.\nTarget audience: Male/Female; 21+ years; Salaried professionals(5L+ PA); Good cibil score (650+)\nValidation Criteria: Card dispatch.\nRegions: Delhi, Chandigarh, Ludhiana, Amritsar, Jalandhar, Kanpur, Lucknow, Jaipur, Jodhpur, Ahmedabad, Vadodra, Surat, Mumbai, Goa, Pune, Nagpur, Indore, Hyderabad, Vishakapatnam, Bangalore, Chennai, Coimbatore, Cochin, Kolkata, Bhubaneswar, Rohtak, Karnal, Agra, Ajmer, Udaipur, Rajkot, Jamnagar, Anand, Valsad, Vapi, Bhopal, Raipur, Vijayawada, Mysore, Hubli, Thiruvananthapuram, Howrah, Durgapur, Asansol, Guwahati, Patna, Allahabad, Varanasi, Bareilly, Meerut, Alwar, Bhilwara, Bikaner, Kolhapur, Jabalpur, Ranga Reddy, Guntur, Tiruchirapalli, Madurai, Kozhikode, Thrissur, Ernakulam, Ranchi, Sonipat, Panipat, Ambala, Yamunanagar, Phagwara, Bhatinda, Nashik, Kharagpur, Siliguri, Jamshedpur, Kurukshetra, Bhubaneswar\r"
+    ,
+    
+        "Indusind Indie": "Target audience: Male/Female; 21+ years; Salaried professionals(5L+PA); Good cibil score (650+)\nValidation Criteria: Account opnes\nRegions: Delhi, Chandigarh, Ludhiana, Amritsar, Jalandhar, Kanpur, Lucknow, Jaipur, Jodhpur, Ahmedabad, Vadodra, Surat, Mumbai, Goa, Pune, Nagpur, Indore, Hyderabad, Vishakapatnam, Bangalore, Chennai, Coimbatore, Cochin, Kolkata, Bhubaneswar, Rohtak, Karnal, Agra, Ajmer, Udaipur, Rajkot, Jamnagar, Anand, Valsad, Vapi, Bhopal, Raipur, Vijayawada, Mysore, Hubli, Thiruvananthapuram, Howrah, Durgapur, Asansol, Guwahati, Patna, Allahabad, Varanasi, Bareilly, Meerut, Alwar, Bhilwara, Bikaner, Kolhapur, Jabalpur, Ranga Reddy, Guntur, Tiruchirapalli, Madurai, Kozhikode, Thrissur, Ernakulam, Ranchi, Sonipat, Panipat, Ambala, Yamunanagar, Phagwara, Bhatinda, Nashik, Kharagpur, Siliguri, Jamshedpur, Kurukshetra, Bhubaneswar"
+    ,
+    
+        "Koskii": null
+    ,
+    
+        "Libas": null
+    ,
+    
+        "Malabar": "No payouts are applicable on Gift Cards.\nGold coin payout is Rs 63/ per gold coin on successful and delivered sale.\nPayout only on the below category:-\n- Diamond and Studded Jewellery: 3.1 %\n- Gold Jewellery and plain gold - 4.4%"
+    ,
+    
+        "Mcaffeine": null
+    ,
+    
+        "Moglix": "Only successful delivered Purchase (No cancellation, refunds, wrong attributions are considered)\nValidation will be done on delivered orders only."
+    ,
+    
+        "Muscleblaze": null
+    ,
+    
+        "Neeman's": "Validation criteria- Successfully delivered Purchase.\nCoupon Not Redeemed: 12.6%\nCoupon Redeemed: 6.3%"
+    ,
+    
+        "Nilkamal": null
+    ,
+    
+        "Nua Woman": null
+    ,
+    
+        "Nutrabay": "Kindly refrain from using any other partnership code, influencer code or codes from others, as such orders will be non-payable.\nRate\tReward\nOther Products\t2.50%\nNutrabay Products\t3.90%\nAtlab Prodcuts\t5.00%"
+    ,
+    
+        "ONLY": null
+    ,
+    
+        "Orange Health": "Step-by-Step Guide:-\n- Visit the Store.\n- Provide your details through the platform.\n- Skilled, trained and vaccinated eMedics will reach you within 60 minutes or as per your chosen time slot.\n- Collected samples will be sent straight to our NABL-accredited and ICMR-approved pathology laboratory for testing.\n- Expect your test reports via WhatsApp & Email within 6 hours, based on the tests done.\n- You can also conveniently access your reports on our app for easy reference.\nRegions: Mumbai, Delhi, Faridabad, Gurugram, Noida, Hyderabad (excluding Bangalore region). "
+    ,
+    
+        "Pantaloons": "Any cancelled or non-fulfilled order will not be eligible for Reward."
+    ,
+    
+        "Pepe Jeans": null
+    ,
+    
+        "Prop Shop24": null
+    ,
+    
+        "realme": "Reward:-\nUpto 15,0000 - 0.60%\n15,000 - 20,000 - 0.09%,\n20,000 - 30,000 - 1.50%,\nAbove 30,000 - 2.00%\nThe SKU's that are available on microsite are billable. The SKU's that are not available on microsite are not billable."
+    ,
+    
+        "ReneeCosmetics": null
+    ,
+    
+        "R for Rabbit": null
+    ,
+    
+        "Salty": "Payout only on successful delivered sales.\nNo Payout on App Orders."
+    ,
+    
+        "SBI CC": "Validation - On Card Issuance/Disbursals"
+    ,
+    
+        "Selected Homme": null
+    ,
+    
+        "Shopclues": "Fashion \t6.9%\tFootwear\t6.3%\tTravel and Luggage\t6.3%\tAutomotive\t3.15%\tSports and Health\t6.3%\tHome & Kitchen\t6.9%\tJewelry & Watches \t5.0%\tBeauty & Perfumes\t5.0%\tToys, Baby & Kids\t5.0%\tBooks & Daily Needs\t0.3%\tMobile & Laptop Accessories \t2.2%\tComputers \t1.8%\tAppliances \t2.3%\tMobile & Tablets \t1.8%\tTV, Audio & Large Appliances \t1.5%\tCameras & Accessories \t1.8%\tBags & luggages \t6.3%\tCameras\t1.8%\tDaily Needs and Essentials \t0.3%"
+    ,
+    
+        "Shriram FD": "Validation Criteria - Pan Verified"
+    ,
+    
+        "Snitch": "New user Purchase: 5.0%\nOld user Purchase: 3.10%"
+    ,
+    
+        "Soulflower": null
+    ,
+    
+        "Superkicks": "Only successfully delivered orders are Rewardable."
+    ,
+    
+        "Tata Neu Credit Card": null
+    ,
+    
+        "The Ayurveda Experience": null
+    ,
+    
+        "The Hindu": "Pay on Subscription"
+    ,
+    
+        "Urban Ladder": "Validation: Only successfully delivered purchases.\nPurchase via us, through their website."
+    ,
+    
+        "Vaaree": "Validation Criteria-Successful Delivered Sales."
+    ,
+    
+        "Veromoda": "On Successful & Valid Purchase"
+    ,
+    
+        "Wellbeing Nutrition": "Old User: 9.5%\nNew User: 15%"
+    ,
+    
+        "W for Woman": null
+    ,
+    
+        "Zouk": null
+    
+};
