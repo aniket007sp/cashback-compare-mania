cc38689c77f9e33855a11f106f2fe0f845ee9b0b
aniket007sp <151561732+aniket007sp@users.noreply.github.com>
2024-12-11 23:25:15 +0530

    Update OfferCategoryPage.jsx

--- src/components/OfferCategoryPage.jsx
+++ src/components/OfferCategoryPage.jsx
@@ -72,7 +72,7 @@ const OfferCategoryPage = () => {
           {toSentenceCase(SubCategory)}
         </h1>
 
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-7 gap-4">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
           {subcategoryOffers.map((offer) => (
             <div
               key={offer.name}
