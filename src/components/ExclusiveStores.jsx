95bb4fe9ef40f9106bd68121679af68bd403ebf7
aniket007sp <151561732+aniket007sp@users.noreply.github.com>
2024-12-15 17:42:10 +0530

    Update ExclusiveStores.jsx

--- src/components/ExclusiveStores.jsx
+++ src/components/ExclusiveStores.jsx
@@ -12,7 +12,7 @@ const ExclusiveStores = () => {
         Exclusive Stores
       </h2>
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-7 gap-4">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {stores.map((store) => (
           <div
             key={store.company}
