7507a20b89d9fb663b81838bb468652b9ba0d237
aniket007sp <151561732+aniket007sp@users.noreply.github.com>
2024-12-15 17:38:20 +0530

    Update NetlifyOffersSection.jsx

--- src/components/NetlifyOffersSection.jsx
+++ src/components/NetlifyOffersSection.jsx
@@ -41,7 +41,7 @@ const NetlifyOffersSection = () => {
                 {category}
               </h3>
             </div>
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-10 gap-4">
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
               {[...data.subcategories].map((subcategory) => (
                 <Link
                   key={subcategory}
