ddf0359ec6fdb4b4da311c13e4fdea82ebabbd19
aniket007sp <151561732+aniket007sp@users.noreply.github.com>
2024-12-31 15:00:04 +0530

    Update Index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -4,33 +4,27 @@ import Footer from '../components/Footer';
 import NetlifyOffersSection from '../components/NetlifyOffersSection';
 import ExclusiveStores from '../components/ExclusiveStores';
 import InstallEarnStores from '../components/Install_and_earn';
-import BrandList from '../components/BrandList'; // Import the BrandList component
-import netlifyOffers from '../data/netlify_offers.json';
-
-const brands = netlifyOffers;
+import BrandList from '../components/BrandList';
+import netlifyOffers from '../data/netlify_offers.json'; // Import JSON
 
 const Index = () => {
   const [activeSection, setActiveSection] = useState('shopping');
 
-  const renderContent = () => {
-    return (
-      <>
-        <ExclusiveStores />
-        <InstallEarnStores />
-        <NetlifyOffersSection />
-        <BrandList brands={brands} />
-        <Footer />
-      </>
-    );
-  };
+  const renderContent = () => (
+    <>
+      <ExclusiveStores />
+      <InstallEarnStores />
+      <NetlifyOffersSection />
+      <Footer />
+    </>
+  );
 
   return (
     <div className="min-h-screen bg-gray-50 flex">
       <Navbar />
       <main className="flex w-full">
-        {/* Left Section: BrandList */}
-        <BrandList brands={brands} />
-
+        {/* Left Section: Brand List */}
+        <BrandList brands={netlifyOffers} />
         {/* Main Content */}
         <section className="flex-1 max-w-[100vw] overflow-x-hidden px-4 sm:px-6 lg:px-8">
           {renderContent()}
