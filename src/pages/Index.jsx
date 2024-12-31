6643f9c796580863818a4760c566e0bdd9628324
aniket007sp <151561732+aniket007sp@users.noreply.github.com>
2024-12-31 14:32:36 +0530

    Update Index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -5,7 +5,9 @@ import NetlifyOffersSection from '../components/NetlifyOffersSection';
 import ExclusiveStores from '../components/ExclusiveStores';
 import InstallEarnStores from '../components/Install_and_earn';
 import BrandList from '../components/BrandList'; // Import the BrandList component
+import netlifyOffers from '../data/netlify_offers.json';
 
+const brands = netlifyOffers;
 
 const Index = () => {
   const [activeSection, setActiveSection] = useState('shopping');
@@ -26,7 +28,7 @@ const Index = () => {
       <Navbar />
       <main className="flex w-full">
         {/* Left Section: BrandList */}
-        <BrandList />
+        <BrandList brands={brands} />
 
         {/* Main Content */}
         <section className="flex-1 max-w-[100vw] overflow-x-hidden px-4 sm:px-6 lg:px-8">
