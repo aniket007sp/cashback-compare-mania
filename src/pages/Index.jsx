0509be549497a73f1baeb5155cb309d2911caefa
aniket007sp <151561732+aniket007sp@users.noreply.github.com>
2025-01-01 10:47:03 +0530

    Update Index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,7 +3,6 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import NetlifyOffersSection from '../components/NetlifyOffersSection';
 import ExclusiveStores from '../components/ExclusiveStores';
-import InstallEarnStores from '../components/Install_and_earn';
 import BrandList from '../components/BrandList';
 import netlifyOffers from '../data/netlify_offers.json'; // Import JSON
 
@@ -13,7 +12,6 @@ const Index = () => {
   const renderContent = () => (
     <>
       <ExclusiveStores />
-      <InstallEarnStores />
       <NetlifyOffersSection />
       <Footer />
     </>
