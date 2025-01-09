451cc7347c4174527b2cc5e0fc8e6254edf698c2
aniket007sp <151561732+aniket007sp@users.noreply.github.com>
2025-01-09 10:36:19 +0530

    Update NetlifyOffersSection.jsx

--- src/components/NetlifyOffersSection.jsx
+++ src/components/NetlifyOffersSection.jsx
@@ -42,7 +42,7 @@ const NetlifyOffersSection = () => {
     <section id="featured-offers" className="container mx-auto px-4 py-8">
       <header>
         <h2 className="text-xl text-white font-semibold mb-4 sm:text-xl bg-[crimson] w-full py-2 text-center">
-          Featured Offers
+          Featured Offersss
         </h2>
       </header>
 
