d592b4b4c614bffcf69579c3c686135e1b90eefa
aniket007sp <151561732+aniket007sp@users.noreply.github.com>
2025-01-13 21:31:16 +0530

    Update SignUp.jsx

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -90,26 +90,6 @@ function SignUp() {
               </div>
             </div>
 
-            {/* Password */}
-            <div className="space-y-2">
-              <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                Password
-              </label>
-              <div className="relative">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <Lock className="h-5 w-5 text-gray-400" />
-                </div>
-                <Input
-                  type="password"
-                  id="password"
-                  name="password"
-                  required
-                  placeholder="••••••••"
-                  className="pl-10"
-                />
-              </div>
-            </div>
-
             {/* Terms and Conditions */}
             <div className="flex items-center">
               <input
