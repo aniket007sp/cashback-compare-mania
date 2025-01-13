0689f75c5858cb15156b57984ce2d8c1c12544b0
aniket007sp <151561732+aniket007sp@users.noreply.github.com>
2025-01-13 15:25:11 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -106,7 +106,7 @@ function Login() {
                 <div className="w-full border-t border-gray-300"></div>
               </div>
               <div className="relative flex justify-center text-sm">
-                <span className="px-2 bg-white text-gray-500">Or continue with</span>
+                <span className="px-2 bg-[crimson] text-white hover:bg-gray-400">Or continue with</span>
               </div>
             </div>
 
