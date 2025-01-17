95d0ab1382afdd26516eb24cd57fb556c53cf851
aniket007sp <151561732+aniket007sp@users.noreply.github.com>
2025-01-17 23:28:47 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,19 +7,19 @@ const Navbar = () => {
   return (
     <nav className="bg-white shadow-md">
       <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-6">
-        <div className="flex items-center justify-between h-16 sm:h-20">
+        <div className="flex items-center justify-between h-16 sm:h-20 md:h-25">
           {/* Logo Section - Smaller on mobile */}
           <a href="/" className="flex-shrink-0">
             <img 
               src={logo} 
               alt="SavvyZi" 
-              className="h-12 w-auto sm:h-16" 
+              className="h-12 w-auto sm:h-16 md:h-20" 
             />
           </a>
 
           {/* Tagline Section - Hidden on very small screens, visible on sm and up */}
           <div className="text-center flex-grow">
-            <div className="text-[crimson] text-xs sm:text-xs md:text-sm lg:text-xl font-semibold whitespace-nowrap">
+            <div className="text-[crimson] text-xs sm:text-xs md:text-lg lg:text-xl font-semibold whitespace-nowrap">
               SavvyZi - <span className="text-[crimson]">Shop Smart, Live Savvy</span>
             </div>
           </div>
@@ -28,7 +28,7 @@ const Navbar = () => {
           <div className="relative z-10">
             <button
               onClick={() => window.open(orderFormUrl, '_blank', 'noopener,noreferrer')}
-              className="bg-[crimson] text-white px-2 sm:px-4 py-1 rounded-md hover:bg-gray-500 transition-colors text-xs sm:text-xs md:text-sm whitespace-nowrap"
+              className="bg-[crimson] text-white px-2 sm:px-4 py-1 rounded-md hover:bg-gray-500 transition-colors text-xs sm:text-xs md:text-md whitespace-nowrap"
             >
               Confirm Order
             </button>
