755c58e15680bc84354e700afae1d3935a45a281
aniket007sp <151561732+aniket007sp@users.noreply.github.com>
2025-01-21 22:13:40 +0530

    Update ExclusiveStores.jsx

--- src/components/ExclusiveStores.jsx
+++ src/components/ExclusiveStores.jsx
@@ -12,7 +12,7 @@ const ExclusiveStores = () => {
         Exclusive Stores
       </h2>
 
-      <div className="flex overflow-x-auto space-x-2 py-2">
+      <div className="flex overflow-x-auto space-x-2 py-2 relative">
         {stores.map((store) => (
           <div
             key={store.COMPANY}
@@ -57,6 +57,9 @@ const ExclusiveStores = () => {
             </button>
           </div>
         ))}
+
+        {/* Gradient indicator */}
+        <div className="absolute top-0 right-0 w-8 h-full bg-gradient-to-l from-white to-transparent pointer-events-none"></div>
       </div>
 
       <Dialog open={!!selectedStore} onOpenChange={() => setSelectedStore(null)}>
@@ -118,8 +121,6 @@ const ExclusiveStores = () => {
 };
 
 export default ExclusiveStores;
-
-
 
 
 // import React, { useState } from 'react';
