14779201883514ef613303f89cecd1be9ac4cd76
aniket007sp <151561732+aniket007sp@users.noreply.github.com>
2025-01-26 16:25:41 +0530

    Update Index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -13,7 +13,7 @@ const Index = () => {
     <Layout>
       <div className="flex flex-col min-h-screen">
         <div className="flex flex-row flex-grow">
-          <div className="w-[120px] sm:w-[200px] min-w-[70px] sticky top-0 h-[calc(100vh-64px)] overflow-y-auto border-r border-gray-200 shrink-0 bg-white shadow-md">
+          <div className="w-[120px] sm:w-[200px] min-w-[70px] sticky top-0 h-[calc(100vh-64px)] overflow-y-auto border-r border-gray-200 shrink-0 bg-[#7E69AB] shadow-md">
             <BrandList brands={netlifyOffers} />
           </div>
           <div className="flex-1 overflow-x-hidden">
