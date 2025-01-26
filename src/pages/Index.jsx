d1b55c55d4ec45b1181cee6fa84d7e847f387c95
aniket007sp <151561732+aniket007sp@users.noreply.github.com>
2025-01-26 17:12:05 +0530

    Update Index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -6,7 +6,7 @@ import ExclusiveStores from '../components/ExclusiveStores';
 import InstallUseEarn from '../components/InstallUseEarn';
 import ExploreUs from '../components/ExploreUs';
 import BrandList from '../components/BrandList';
-import netlifyOffers from '../data/latest/fashion.json';
+import brands from '../data/latest/brands.json';
 
 const Index = () => {
   return (
@@ -14,7 +14,7 @@ const Index = () => {
       <div className="flex flex-col min-h-screen">
         <div className="flex flex-row flex-grow">
           <div className="w-[120px] sm:w-[200px] min-w-[70px] sticky top-0 h-[calc(100vh-64px)] overflow-y-auto border-r border-gray-200 shrink-0 bg-[#7E69AB] shadow-md">
-            <BrandList brands={netlifyOffers} />
+            <BrandList brands={brands} />
           </div>
           <div className="flex-1 overflow-x-hidden">
             <ExclusiveStores />
