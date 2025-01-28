abb4d04c9a313618f6fa15978a349d89e5ac53fa
aniket007sp <151561732+aniket007sp@users.noreply.github.com>
2025-01-28 15:15:26 +0530

    Update InstallUseEarn.jsx

--- src/components/InstallUseEarn.jsx
+++ src/components/InstallUseEarn.jsx
@@ -81,7 +81,7 @@ const InstallUseEarn = () => {
           onClick={scrollRight}
         >
           <div className="w-5 h-5 md:w-10 md:h-10 bg-[crimson] rounded-full flex items-center justify-center animate-blink">
-            <div className="text-[crimson] text-xl">&rarr;</div>
+            <div className="text-gray-200 text-xl">&rarr;</div>
           </div>
         </div>
       </div>
